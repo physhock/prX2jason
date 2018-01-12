04f316eeb49bba179307bad6ae9e9f6eafbfd60a
Fedor <frerom@yandex.ru>
2018-01-12 14:30:06 +0300

    fixed err msg, close #6

--- src/main/java/Validator.java
+++ src/main/java/Validator.java
@@ -13,7 +13,7 @@ public class Validator implements Server {
     @NotNull
     private final Gson builder;
 
-    private static final int PORT = 80;
+    private static final int PORT = 8080;
     private static final int CODE_OK = 200;
     private static final String ROOT = "/";
 
@@ -36,7 +36,7 @@ public class Validator implements Server {
                                 e.hashCode(),
                                 errorSplittedString[1],
                                 "at " + errorSplittedString[2],
-                                jsonRequest,
+                                http.getRequestURI().getPath(),
                                 request_id
                         ));
             } finally {
